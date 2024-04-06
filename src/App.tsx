9826237cb4b7519233746e4c5a9894028022e46b
sahka_sharit <alexsharit13@gmail.com>
2024-04-06 11:29:16 +0500

    Create first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import ParamEditor from './components/ParamEditor';
+
+export type Param = {
+  id: number;
+  name: string;
+}
+
+export type ParamValue = {
+  paramId: number;
+  value: string;
+}
+
+export type Model = {
+  paramValues: ParamValue[];
+}
 
 function App() {
+
+  const params: Param[] = [
+    { id: 1, name: 'Назначение' },
+    { id: 2, name: 'Длина' },
+  ]
+
+  const initialModel: Model = {
+    'paramValues': [
+      {
+        'paramId': 1,
+        'value': 'повседневное',
+      },
+      {
+        'paramId': 2,
+        'value': 'макси',
+      },
+    ]
+  }
+
+  const [model, setModel] = useState<Model>(initialModel);
+
+  const handleUpdateModel = (updateModel: Model) => {
+    setModel(updateModel);
+  }
+
+  const getModel = () => {
+    return model;
+  }
+  console.log(getModel());
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div >
+      <ParamEditor params={params} model={model} onUpdateModel={handleUpdateModel} />
+    </div >
   );
 }
+
 
 export default App;
